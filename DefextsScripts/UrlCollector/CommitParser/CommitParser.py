de8aeae0817feea45fc2dc8d5148cd76e17f7251
Samuel Benton <ProdigyXable@users.noreply.github.com>
2021-05-26 07:32:14 -0500

    Parsed configuration input file

--- DefextsScripts/UrlCollector/CommitParser/CommitParser.py
+++ DefextsScripts/UrlCollector/CommitParser/CommitParser.py
@@ -7,12 +7,28 @@ class CommitParser (object):
     
     THREADS = 2
 
+    OUTPUT_DIRECTORY = None
+    DATASET_FILEPATH = None
+    TEMP_FILEPATH = None
+
     logger = None
 
     def __init__(self, verbose, filepath):
         self.logger = Log(verbose)
         self.logger.info("Loading configuration details from {}".format(filepath))
+        
+        for keyword in self.CRITERIA_KEYWORDS:
+            self.logger.info("Queried keyword: {}".format(keyword))
 
+        config_file = open(filepath, "r")
+        configuration_data = config_file.readlines()
+        assert len(configuration_data) >= 2, "Invalid content of {}".format(filepath)
+        
+        self.OUTPUT_DIRECTORY = configuration_data[0].strip()
+        self.logger.detailed("Output directory = {}".format(self.OUTPUT_DIRECTORY))
+        
+        self.DATASET_FILEPATH = configuration_data[1].strip()
+        self.logger.detailed("Input filepath = {}".format(self.DATASET_FILEPATH))
 
     def begin(self):
         pass
