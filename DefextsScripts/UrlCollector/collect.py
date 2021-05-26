072cfe08f9f6fb1c6132b1a7db5a45aa35865480
Samuel Benton <ProdigyXable@users.noreply.github.com>
2021-05-25 23:32:53 -0500

    Improved project messages

--- DefextsScripts/UrlCollector/collect.py
+++ DefextsScripts/UrlCollector/collect.py
@@ -13,8 +13,8 @@ if( os.path.exists(access_file_name) ):
     github_username = details[0].strip()
     access_token = details[1].strip()
 else:
-    github_username = input("Input your Github username (needed to increase the number of results):\t").strip()
-    access_token = getpass.getpass("Input access token: \t").strip()
+    github_username = input("Input your Github username (authenticated users may request more per minute):\t").strip()
+    access_token = getpass.getpass("Input password / access token: \t").strip()
 
 LANGUAGES_CONFIGURATION = [ 'Kotlin', 'Scala', 'Groovy', 'Closure', 'Jython', 'JRuby', 'Java' ]
 
