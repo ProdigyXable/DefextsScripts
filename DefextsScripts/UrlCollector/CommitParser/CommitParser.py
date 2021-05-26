dce9f3d8b4bfbedfe557acbebe5e20b0696d99fc
Samuel Benton <ProdigyXable@users.noreply.github.com>
2021-05-26 07:39:06 -0500

    Adjusted keywords and added input sanity checks

--- DefextsScripts/UrlCollector/CommitParser/CommitParser.py
+++ DefextsScripts/UrlCollector/CommitParser/CommitParser.py
@@ -1,9 +1,10 @@
 from HelperUtility.log import Log
+import os
 
 class CommitParser (object):
     """Class to parse all of a repository's commit, looking for those satisfying certain criteria"""
 
-    CRITERIA_KEYWORDS = [ "fix", "remove", "error", "repair", "issue", "resolve", "solve" ]
+    CRITERIA_KEYWORDS = [ "fix", "add", "change", "modify" "remove", "error", "repair", "issue", "resolve", "solve" ]
     
     THREADS = 2
 
@@ -18,17 +19,21 @@ class CommitParser (object):
         self.logger.info("Loading configuration details from {}".format(filepath))
         
         for keyword in self.CRITERIA_KEYWORDS:
-            self.logger.info("Queried keyword: {}".format(keyword))
+            self.logger.info("Queried keyword: {}".format(keyword.lower()))
 
         config_file = open(filepath, "r")
         configuration_data = config_file.readlines()
         assert len(configuration_data) >= 2, "Invalid content of {}".format(filepath)
         
         self.OUTPUT_DIRECTORY = configuration_data[0].strip()
+        os.makedirs(self.OUTPUT_DIRECTORY, exist_ok = True)
         self.logger.detailed("Output directory = {}".format(self.OUTPUT_DIRECTORY))
         
         self.DATASET_FILEPATH = configuration_data[1].strip()
+        assert os.path.exists(self.DATASET_FILEPATH), "Invalid input filepath specified: {}".format(self.DATASET_FILEPATH)
         self.logger.detailed("Input filepath = {}".format(self.DATASET_FILEPATH))
 
     def begin(self):
-        pass
+        input_file = open(self.DATASET_FILEPATH, "r")
+        input_data = input_file.readlines()
+        self.logger.info("{} projects detected".format(len(input_data)))
