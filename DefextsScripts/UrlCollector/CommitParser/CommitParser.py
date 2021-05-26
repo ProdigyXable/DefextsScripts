6366aff1faab74f3b8ac889d6510aedc36266362
Samuel Benton <ProdigyXable@users.noreply.github.com>
2021-05-26 07:00:58 -0500

    Added entry point file for CommitParser

--- DefextsScripts/UrlCollector/CommitParser/CommitParser.py
+++ DefextsScripts/UrlCollector/CommitParser/CommitParser.py
@@ -1,3 +1,13 @@
+from HelperUtility import log
+
 class CommitParser (object):
     """Class to parse all of a repository's commit, looking for those satisfying certain criteria"""
 
+    CRITERIA_KEYWORDS = [ "fix", "remove", "error", "repair", "issue", "resolve", "solve" ]
+    
+    THREADS = 2
+
+    logger = None
+
+    def __init__(self, verbose, filepath):
+        self.logger = log(verbose)
--- DefextsScripts/UrlCollector/CommitParser/parse.py
+++ DefextsScripts/UrlCollector/CommitParser/parse.py
@@ -1,1 +1,11 @@
+import os
+import CommitParser
+from HelperUtility.log import VerboseLevel
 
+# --- Start --- #
+configuration_details = "details.configuration"
+
+if os.path.exists(configuration_details):
+    cp = CommitParser(VerboseLevel.DEBUG, configuration_details)
+else:
+    print("Missing configuration file. Program exiting")
